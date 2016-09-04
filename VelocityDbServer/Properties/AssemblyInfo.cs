a66b811bc63948c93fb1b3ae05accf17a1351944
Mats Persson <Mats@VelocityDB.com>
2016-09-04 09:44:18 -0700

    Version 5.6.3

---
+++ VelocityDbServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VelocityDbServer")]
+[assembly: AssemblyDescription("VelocityDbServer provides locking services as well as page delivery and backup services")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VelocityDB, Inc.")]
+[assembly: AssemblyProduct("VelocityDbServer")]
+[assembly: AssemblyCopyright("Copyright © 2016 VelocityDB, Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d03ebf90-7558-45af-b75d-20c87d00190c")]
+
+// VersionNumber information for an assembly consists of the following four values:
+//
+//      Major VersionNumber
+//      Minor VersionNumber 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("5.5.1.0")]
+[assembly: AssemblyFileVersion("5.5.1.0")]
---
+++ VelocityDbServer/Service.cs
@@ -0,0 +1,160 @@
+﻿/* Copyright © VelocityDB, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Mats Persson mailto:Mats@VelocityDB.com, May 2013
+ */
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using VelocityDb.Session;
+using VelocityDB.Server;
+using System.Runtime.InteropServices;
+#if !NET_CORE
+using System.ServiceProcess;
+#endif
+
+namespace VelocityDb.Server
+{
+  public partial class Service
+#if !NET_CORE
+    : ServiceBase
+#endif
+  {
+    public static Thread tcpListenThread;
+    public static TcpListener listener = null;
+    public static bool stopService = false;
+    public Service()
+    {
+ #if !NET_CORE
+      InitializeComponent();
+#endif
+    }
+
+    public static void CreateAndStartThreads(bool asService, int numberOfWorkerThreads)
+    {
+      Thread.CurrentThread.Name = "VelocityDBServer Main Thread";
+      if (asService)
+      {
+        tcpListenThread = new Thread(new ThreadStart(tcpListen));
+        tcpListenThread.Name = "VelocityDBServer Listener";
+        //Thread.Sleep(1000);
+        tcpListenThread.Start();
+      }
+      else
+        tcpListen();
+    }
+
+    protected static void tcpListen()
+    {
+      try
+      {
+        //IPAddress ipAddress = Dns.Resolve("localhost").AddressList[0];
+        //listener = new TcpListener(ipAddress, ServerTcpClient.odbTcpPort);
+        //listener = new TcpListener(IPAddress.IPv6Any, ServerTcpClient.odbTcpPort);
+        listener = new TcpListener(IPAddress.Any, SessionBase.s_serverTcpIpPortNumber);
+        Socket s = listener.Server;
+        LingerOption lingerOption = new LingerOption(true, 0);
+        s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Linger, lingerOption);
+        s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+        s.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, 1);
+        listener.Start();
+      }
+      catch (System.Exception e)
+      {
+        ServerTcpClient.s_odbServerLog.WriteEntry(e.ToString());
+      }
+
+      try
+      {
+        while (!ServerTcpClient.ShutDown)
+        {
+          ServerTcpClient.s_acceptDone.Reset();
+#if NET_CORE
+          //listener?.AcceptSocketAsync().Wait();
+          ServerTcpClient.AcceptTcpClient(listener);
+#else
+          listener?.BeginAcceptTcpClient(new AsyncCallback(ServerTcpClient.AcceptTcpClient), listener);
+#endif
+
+          ServerTcpClient.s_acceptDone.WaitOne();
+        }
+      }
+      catch (SocketException e)
+      {
+#if !NET_CORE
+        if (e.ErrorCode != 10054)	// client closed socket
+#endif
+        {
+          ServerTcpClient.s_odbServerLog.WriteEntry(e.ToString());
+        }
+      }
+      catch (System.Exception e)
+      {
+        ServerTcpClient.s_odbServerLog.WriteEntry(e.ToString());
+      }
+      finally
+      {
+        listener.Stop();
+      }
+
+      if (!stopService)
+      {
+        try
+        {
+          listener?.Stop();
+          Environment.Exit(0);
+        }
+        catch (System.Exception e)
+        {
+          ServerTcpClient.s_odbServerLog.WriteEntry(e.ToString());
+        }
+      }
+    }
+#if !NET_CORE
+    protected override void OnStart(string[] args)
+    {
+      //Thread.Sleep(15000); // wait for possible prior service
+      stopService = false;
+      ServerTcpClient.s_odbServerLog.Source = "VelocityDbServer";
+      ServerTcpClient.ShutDown = false;
+      CreateAndStartThreads(true, ServerTcpClient.s_numberOfWorkerThreads);
+    }
+
+    protected override void OnStop()
+    {
+      stopService = true;
+      ServerTcpClient.ShutDown = true;
+      ServerTcpClient.s_acceptDone.Set();
+      System.Environment.Exit(0); // fast shutdown so that reinstall succeeds (?)
+      tcpListenThread.Join();
+    }
+#endif
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern bool FreeConsole();
+
+    [DllImport("kernel32", SetLastError = true)]
+    static extern bool AttachConsole(int dwProcessId);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+    public void startAsNonService(string[] args)
+    {
+      IntPtr ptr = GetForegroundWindow();
+      int u;
+      GetWindowThreadProcessId(ptr, out u);
+      Process process = Process.GetProcessById(u);
+      if (process.ProcessName == "cmd")
+        AttachConsole(process.Id);
+      ServerTcpClient.s_odbServerLog.Source = "VelocityDbServer";
+      ServerTcpClient.ShutDown = false;
+      CreateAndStartThreads(false, ServerTcpClient.s_numberOfWorkerThreads);
+      FreeConsole();
+    }
+  }
+}
