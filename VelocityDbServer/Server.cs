b91f69c46489ce95c029c741ca43381c5998f1e2
Mats Persson <Mats@VelocityDB.com>
2016-09-04 09:44:18 -0700

    Version 5.6.3

---
+++ VelocityDbServer/Server.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+#if !NET_CORE
+using System.Configuration;
+using System.Configuration.Install;
+using System.ServiceProcess;
+#endif
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using VelocityDb.Session;
+using VelocityDB.Server;
+
+namespace VelocityDb.Server
+{
+	static class Server
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+      bool startAsNonService = false;
+ #if !NET_CORE
+			// Create the source, if it does not already exist.
+			if (!EventLog.SourceExists("VelocityDbServer"))
+			{
+				//An event log source should not be created and immediately used.
+				//There is a latency time to enable the source, it should be create
+				//prior to executing the application that uses the source.
+				//Execute this sample a second time to use the new source.
+        EventLog.CreateEventSource("VelocityDbServer", "VelocityDbServerLog");
+			}
+#endif
+      if (args.Length > 0)
+      {
+        if (bool.TryParse(args[0], out startAsNonService) == false)
+          Console.WriteLine("First parameter must be a boolean (0/1) stating if server should shutdown as non service");
+      }
+#if NET_CORE
+      string serverLogFile = "c:/serverCoreLog.txt";
+#else
+      string serverLogFile = ConfigurationManager.AppSettings["ServerActivityLogFile"] ?? "" .ToString();
+#endif
+      if (serverLogFile.Length > 0)
+      {
+        try
+        {
+          FileInfo info = new FileInfo(serverLogFile);
+          Stream s = info.Open(FileMode.Append, FileAccess.Write, FileShare.Read);
+          if (s != null)
+            ServerTcpClient.s_serverActivityLogFile = new StreamWriter(s);
+        }
+        catch
+        {
+
+        }
+      }
+#if !NET_CORE
+      string baseDatabasePath = ConfigurationManager.AppSettings["BaseDatabasePath"]  ?? "" .ToString();
+      if (baseDatabasePath.Length > 0)
+        SessionBase.BaseDatabasePath = baseDatabasePath;
+
+      string doWindowsAuth = ConfigurationManager.AppSettings["DoWindowsAuthentication"] ?? "" .ToString();
+      bool doWindowsAuthentication;
+      if (bool.TryParse(doWindowsAuth, out doWindowsAuthentication))
+        SessionBase.DoWindowsAuthentication = doWindowsAuthentication;
+      string workerThreadCt = ConfigurationManager.AppSettings["NumberOfWorkerThreads"]  ?? "" .ToString();
+      int.TryParse(workerThreadCt, out ServerTcpClient.s_numberOfWorkerThreads);
+      string portNumber = ConfigurationManager.AppSettings["TcpIpPortNumber"] ?? "".ToString();
+      int.TryParse(portNumber, out SessionBase.s_serverTcpIpPortNumber);
+      string maximumMemoryUseStr = ConfigurationManager.AppSettings["MaximumMemoryUse"] ?? "".ToString();
+      long maximumMemoryUse = 0;
+      long.TryParse(maximumMemoryUseStr, out maximumMemoryUse);
+      if (maximumMemoryUse > 0)
+
+
+        DataCache.MaximumMemoryUse = maximumMemoryUse;
+#if !BLOCKINGQueue
+      ServerTcpClient.s_requestRingBuffer.SetGatingSequences();
+#endif
+			if (!startAsNonService)
+			{
+				ServiceBase[] ServicesToRun;
+				ServicesToRun = new ServiceBase[] 
+				{ 
+					new Service() 
+				};
+				ServiceBase.Run(ServicesToRun);
+			}
+			else
+#endif
+			{
+      Service velocityDbServer = new Service();
+        velocityDbServer.startAsNonService(args);
+			}
+		}
+	}
+}
---
+++ xUnitTests/Sanity.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using VelocityDb;
+using VelocityDb.Collection.BTree;
+using VelocityDb.Session;
+using Xunit;
+
+namespace xUnitTests
+{
+  public class ADatabaseIteration
+  {
+    static readonly string drive = "c:\\";
+    static readonly string licenseDbFile = Path.Combine(drive, "4.odb");
+    public const string systemDir = "c:/NUnitTestDbs";
+    public const string location2Dir = "c:/NUnitTestDbsLocation2";
+
+    [Fact]
+    public void PassingTest()
+    {
+      Assert.Equal(4, Add(2, 2));
+    }
+
+    int Add(int x, int y)
+    {
+      return x + y;
+    }
+
+    [Fact]
+    public void aaaDeleteDatabasesFromPriorRun()
+    {
+      SessionBase.DefaultCompressPages = PageInfo.compressionKind.LZ4;
+
+      if (Directory.Exists(systemDir))
+      {
+        foreach (string s in Directory.GetFiles(systemDir))
+          File.Delete(s);
+        foreach (string s in Directory.GetDirectories(systemDir))
+          Directory.Delete(s, true);
+      }
+      else
+        Directory.CreateDirectory(systemDir);
+      if (Directory.Exists(location2Dir))
+      {
+        foreach (string s in Directory.GetFiles(location2Dir))
+          File.Delete(s);
+        foreach (string s in Directory.GetDirectories(location2Dir))
+          Directory.Delete(s, true);
+      }
+      else
+        Directory.CreateDirectory(location2Dir);
+    }
+
+    [Fact]
+    public void aaaFakeLicenseDatabase()
+    {
+      Assert.Throws<NoValidVelocityDBLicenseFoundException>(() =>
+      {
+        using (SessionNoServer session = new SessionNoServer(systemDir))
+        {
+          session.BeginUpdate();
+          Database database;
+          License license = new License("Mats", 1, null, null, null, 99999, DateTime.MaxValue, 9999, 99, 9999);
+          Placement placer = new Placement(License.PlaceInDatabase, 1, 1, 1);
+          license.Persist(placer, session);
+          for (uint i = 10; i < 20; i++)
+          {
+            database = session.NewDatabase(i);
+            Assert.NotNull(database);
+          }
+          session.Commit();
+          File.Copy(Path.Combine(systemDir, "20.odb"), Path.Combine(systemDir, "4.odb"));
+          session.BeginUpdate();
+          for (uint i = 21; i < 30; i++)
+          {
+            database = session.NewDatabase(i);
+            Assert.NotNull(database);
+          }
+          session.RegisterClass(typeof(BTreeSet<int>));
+          session.RegisterClass(typeof(BTreeSet<long>));
+          session.RegisterClass(typeof(BTreeSet<DateTime>));
+          session.RegisterClass(typeof(BTreeSet<double>));
+          session.RegisterClass(typeof(BTreeMap<string, double>));
+          session.Commit();
+        }
+      });
+    }
+
+    [Theory]
+    [InlineData(true, true)]
+    public void aaaFakeLicenseDatabaseCleanup(bool deleteLocationProperly, bool useServerSession)
+    {
+      if (deleteLocationProperly)
+        using (SessionBase session = useServerSession ? (SessionBase)new ServerClientSession(systemDir) : (SessionBase)new SessionNoServer(systemDir))
+        {
+          session.BeginUpdate();
+          DatabaseLocation defaultLocation = session.DatabaseLocations.Default();
+          List<Database> dbList = session.OpenLocationDatabases(defaultLocation, true);
+          foreach (Database db in dbList)
+            if (db.DatabaseNumber > Database.InitialReservedDatabaseNumbers)
+              session.DeleteDatabase(db);
+          session.DeleteLocation(defaultLocation);
+          session.Commit();
+        }
+      foreach (string s in Directory.GetFiles(systemDir))
+        File.Delete(s);
+      using (SessionBase session = useServerSession ? (SessionBase)new ServerClientSession(systemDir) : (SessionBase)new SessionNoServer(systemDir))
+      {
+        session.BeginUpdate();
+        UInt32 dbNum = session.DatabaseNumberOf(typeof(NotSharingPage));
+        session.NewDatabase(dbNum);
+        session.Commit();
+      }
+      File.Copy(licenseDbFile, Path.Combine(systemDir, "4.odb"));
+    }
+  }
+
+  public class NotSharingPage : OptimizedPersistable
+  {
+    static long ct = 0;
+    long someData;
+    public NotSharingPage()
+    {
+      someData = ++ct;
+    }
+
+    public override bool AllowOtherTypesOnSamePage
+    {
+      get
+      {
+        return false;
+      }
+    }
+  }
+}
