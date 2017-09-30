88600f7744dcde40a8c4d0f043cf95d6e56ee6f3
VelocityDB <Mats@VelocityDB.com>
2017-09-29 21:54:01 -0700

    Version 6.9

--- UpdateClass/UpdateClass.cs
+++ UpdateClass/UpdateClass.cs
@@ -5,7 +5,6 @@ using System.Text;
 using VelocityDb;
 using VelocityDb.TypeInfo;
 using VelocityDb.Session;
-using VelocityDbSchema.Samples.UpdateClass;
 using System.IO;
 using System.Diagnostics;
 
@@ -14,7 +13,7 @@ namespace UpdateClass
   /// <summary>
   /// Test updating a class that already exist in the Database schema, rerun multiple times. Modify class in between runs. Check objects by browsing them with VelocityDbBrowser
   /// </summary>
-  public class UpdateClass
+  public class UpdatesClass
   {
     static readonly string s_systemDir = "UpdateClass"; // appended to SessionBase.BaseDatabasePath
 
@@ -22,14 +21,16 @@ namespace UpdateClass
     {
       try
       {
-        UpdatedClass updatedClassObject;
+        Trace.Listeners.Add(new ConsoleTraceListener());
+        VelocityDbSchema.Samples.UpdateClass.UpdatedClass updatedClassObject;
         int ct1 = 0;
         using (SessionNoServer session = new SessionNoServer(s_systemDir))
         {
+          session.SetTraceDbActivity(Schema.SchemaDB);
           session.BeginUpdate();
-          session.UpdateClass(typeof(UpdatedClass)); // call this when you have updated the class since first storing instances of this type or since last call to UpdateClass
-          UInt32 dbNum = session.DatabaseNumberOf(typeof(UpdatedClass));
-          foreach (UpdatedClass obj in session.AllObjects<UpdatedClass>())
+          session.UpdateClass(typeof(VelocityDbSchema.Samples.UpdateClass.UpdatedClass)); // call this when you have updated the class since first storing instances of this type or since last call to UpdateClass
+          UInt32 dbNum = session.DatabaseNumberOf(typeof(VelocityDbSchema.Samples.UpdateClass.UpdatedClass));
+          foreach (var obj in session.AllObjects<VelocityDbSchema.Samples.UpdateClass.UpdatedClass>())
           {
             Console.Write(obj.ToString() + " has members: ");
             foreach (DataMember member in obj.GetDataMembers())
@@ -43,11 +44,77 @@ namespace UpdateClass
           int ct2 = 0;
           Database db = session.OpenDatabase(dbNum, true, false);
           if (db != null)
-            foreach (UpdatedClass obj in db.AllObjects<UpdatedClass>())
+            foreach (var obj in db.AllObjects<VelocityDbSchema.Samples.UpdateClass.UpdatedClass>())
               ct2++;
-          Debug.Assert(ct1 == ct2);         
-          updatedClassObject = new UpdatedClass();
+          Debug.Assert(ct1 == ct2);
+          updatedClassObject = new VelocityDbSchema.Samples.UpdateClass.UpdatedClass();
           session.Persist(updatedClassObject);
+          session.Commit();
+          MoveToDifferentFullClassName();
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+        return 1;
+      }
+      return 0;
+    }
+
+    static int MoveToDifferentFullClassName()
+    {
+      try
+      {
+        using (SessionNoServer session = new SessionNoServer(s_systemDir))
+        {
+          session.SetTraceDbActivity(Schema.SchemaDB);
+          session.BeginUpdate();
+          session.ReplacePersistedType(typeof(VelocityDbSchema.Samples.UpdateClass.UpdatedClass), typeof(UpdateClass.UpdatedClass));
+          session.Commit();
+        }
+        using (SessionNoServer session = new SessionNoServer(s_systemDir))
+        {
+          session.Verify();
+          session.BeginRead();
+          foreach (var obj in session.AllObjects<UpdateClass.UpdatedClass>())
+          {
+            Console.Write(obj.ToString() + " has members: ");
+            foreach (DataMember member in obj.GetDataMembers())
+            {
+              Console.Write(member.ToString() + " ");
+            }
+          }
+          foreach (var obj in session.AllObjects<VelocityDbSchema.Samples.UpdateClass.UpdatedClass>())
+          {
+            throw new Exception("not expected");
+          }
+          session.Commit();
+        }
+
+        // change it back to original
+        using (SessionNoServer session = new SessionNoServer(s_systemDir))
+        {
+          session.SetTraceDbActivity(Schema.SchemaDB);
+          session.BeginUpdate();
+          session.ReplacePersistedType(typeof(UpdateClass.UpdatedClass).AssemblyQualifiedName, typeof(VelocityDbSchema.Samples.UpdateClass.UpdatedClass));
+          session.Commit();
+        }
+        using (SessionNoServer session = new SessionNoServer(s_systemDir))
+        {
+          session.Verify();
+          session.BeginRead();
+          foreach (var obj in session.AllObjects<VelocityDbSchema.Samples.UpdateClass.UpdatedClass>())
+          {
+            Console.Write(obj.ToString() + " has members: ");
+            foreach (DataMember member in obj.GetDataMembers())
+            {
+              Console.Write(member.ToString() + " ");
+            }
+          }
+           foreach (var obj in session.AllObjects<UpdateClass.UpdatedClass>())
+           {
+            throw new Exception("not expected");
+          }
           session.Commit();
         }
       }
@@ -57,6 +124,7 @@ namespace UpdateClass
         return 1;
       }
       return 0;
+
     }
   }
 }
---
+++ UpdateClass/UpdatedClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VelocityDb;
+
+namespace UpdateClass
+{
+  public class UpdatedClass : OptimizedPersistable
+  {
+#pragma warning disable 0414
+    //UpdatedClass aRef;
+    //Int64 aInt64; 
+    //UInt16 aUint16; 
+    UInt32 aUint32;
+    UInt64 aUint64;
+    //string aString;
+    float aFloat;
+#pragma warning restore 0414
+    //Int32[] intArray;
+    //DateTime aDateTime;
+
+    public UpdatedClass()
+    {
+      //aRef = this;
+      //aInt64 = Int64.MaxValue;
+      //aUint16 = UInt16.MaxValue;
+      aUint32 = UInt32.MaxValue;
+      aUint64 = UInt64.MaxValue;
+      //aString = DateTime.Now.ToString();
+      aFloat = float.MaxValue;
+      //intArray = new Int32[10];
+      //aDateTime = DateTime.Now;
+    }
+  }
+}
