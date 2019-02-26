e16b2eb20573c6fc942dfe6a2b94dc3c96efb485
VelocityDB <Mats@VelocityDB.com>
2019-02-26 11:49:19 -0800

    Version 8.1.2

--- VelocityGraph/Properties/AssemblyInfo.cs
+++ VelocityGraph/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VelocityGraph")]
-[assembly: AssemblyDescription("Graph Database API")]
+[assembly: AssemblyTitle("VelocityGraph2")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VelocityDB, Inc")]
-[assembly: AssemblyProduct("VelocityGraph")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VelocityGraph2")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e77a1f62-c2b6-4be6-ab0d-34a9c677ccf1")]
+[assembly: Guid("455ab0e1-e23b-4547-8626-c064a76b7e4c")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.1.0.0")]
-[assembly: AssemblyFileVersion("8.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
